f327d4f01040b643c84484eed5572c3a1815ec8d
Gabor <czghun@gmail.com>
2017-12-04 08:49:35 +0100

    link changes to requestmapping

--- src/main/java/hu/czghun/archive/controller/ArchiveController.java
+++ src/main/java/hu/czghun/archive/controller/ArchiveController.java
@@ -6,9 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ArchiveController {
 
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
 	@RequestMapping("/")
 	public String mainPage() {
 		return "index";
+	}
+	
+	@RequestMapping("/vulnerability")
+	public String vulnerability() {
+		return "vulnerability";
+	}
+	
+	@RequestMapping("/attacks")
+	public String attacks() {
+		return "attacks";
 	}
 	
 }
